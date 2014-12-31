a8f1cd5c25890bef232aa7832454ea2f12b6866d
Jonas Auer <jonas.auer@gmx.de>
2014-12-31 19:04:39 +0100

    Added prototype for the main window, made the whole application executable

--- Application/src/usspg31/tourney/controller/EntryPoint.java
+++ Application/src/usspg31/tourney/controller/EntryPoint.java
@@ -1,10 +1,32 @@
 package usspg31.tourney.controller;
 
-public class EntryPoint {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class EntryPoint extends Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader();
+			fxmlLoader.setLocation(this.getClass().getResource("/ui/fxml/main-window.fxml"));
+			Parent root = null;
+			root = (Parent)fxmlLoader.load();
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Tourney");
+
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
