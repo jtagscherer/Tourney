b0919f65df100c17293635097acceaed4d95928e
Jonas Auer <jonas.auer@gmx.de>
2015-01-03 18:58:30 +0100

    Changed the EntryPoint to use the java logger

--- Application/src/usspg31/tourney/controller/EntryPoint.java
+++ Application/src/usspg31/tourney/controller/EntryPoint.java
@@ -1,5 +1,7 @@
 package usspg31.tourney.controller;
 
+import java.util.logging.Logger;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,12 +10,25 @@ import javafx.stage.Stage;
 
 public class EntryPoint extends Application {
 
+	private static final Logger log = Logger.getLogger(EntryPoint.class.getName());
+
+	private static EntryPoint instance = null;
+
 	public static void main(String[] args) {
-		launch(args);
+		log.info("Starting Application");
+		log.info("Running JavaFX Version " + System.getProperty("javafx.runtime.version"));
+
+		try {
+			launch(args);
+		} catch (Throwable t) {
+			log.severe(t.toString());
+		}
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		instance = this;
+
 		try {
 			FXMLLoader fxmlLoader = new FXMLLoader();
 			fxmlLoader.setLocation(this.getClass().getResource("/ui/fxml/main-window.fxml"));
@@ -25,7 +40,7 @@ public class EntryPoint extends Application {
 
 			primaryStage.show();
 		} catch(Exception e) {
-			e.printStackTrace();
+			log.severe(e.toString());
 		}
 	}
 
