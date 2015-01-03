d4de2f5ad891f79b439511c1c477113c7e2787ee
Fabian Biester <fabian@familie-biester.de>
2015-01-03 17:38:53 +0100

    added getter and setter as pictured in the uml class diagram

--- Application/src/usspg31/tourney/model/Person.java
+++ Application/src/usspg31/tourney/model/Person.java
@@ -1,5 +1,6 @@
 package usspg31.tourney.model;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 public class Person {
@@ -8,4 +9,46 @@ public class Person {
 	private final StringProperty lastName;
 	private final StringProperty mailAddress;
 
+	public Person() {
+		firstName = new SimpleStringProperty();
+		lastName = new SimpleStringProperty();
+		mailAddress = new SimpleStringProperty();
+
+	}
+
+	public String getFirstName() {
+		return this.firstName.get();
+	}
+
+	public void setFirstName(String value) {
+		this.firstName.set(value);
+	}
+
+	public StringProperty firstNameProperty() {
+		return this.firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName.get();
+	}
+
+	public void setLastName(String value) {
+		this.lastName.set(value);
+	}
+
+	public StringProperty lastNameProperty() {
+		return this.lastName;
+	}
+
+	public String getMailAddress() {
+		return this.mailAddress.get();
+	}
+
+	public void setMailAdress(String value) {
+		this.mailAddress.set(value);
+	}
+
+	public StringProperty mailAdressProperty() {
+		return this.mailAddress;
+	}
 }
