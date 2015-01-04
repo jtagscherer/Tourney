0862c0e818966415896a392ab580b5e080d85668
Jonas Auer <jonas.auer@gmx.de>
2015-01-04 18:36:10 +0100

    Corrected the logging calls in the EntryPoint Added Logger to the OptionsViewController

--- Application/src/usspg31/tourney/controller/controls/OptionsViewController.java
+++ Application/src/usspg31/tourney/controller/controls/OptionsViewController.java
@@ -1,11 +1,15 @@
 package usspg31.tourney.controller.controls;
 
+import java.util.logging.Logger;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import usspg31.tourney.controller.MainWindow;
 
 
 public class OptionsViewController {
+
+	private final Logger log = Logger.getLogger(OptionsViewController.class.getName());
 
 	@FXML private Button buttonChangeLanguage;
 	@FXML private Button buttonChangePassword;
