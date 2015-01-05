1a890d6a80f72592430591b4b0d98726d16044b8
Fabian Biester <fabian@familie-biester.de>
2015-01-05 19:43:36 +0100

    finished the constructors

--- Application/src/usspg31/tourney/model/GamePhase.java
+++ Application/src/usspg31/tourney/model/GamePhase.java
@@ -2,12 +2,10 @@ package usspg31.tourney.model;
 
 import java.time.Duration;
 
-import javafx.beans.InvalidationListener;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+import javafx.beans.property.SimpleObjectProperty;
 import usspg31.tourney.model.pairingstrategies.PairingStrategy;
 
 public class GamePhase {
@@ -22,142 +20,7 @@ public class GamePhase {
 		this.cutoff = new SimpleIntegerProperty();
 		this.roundCount = new SimpleIntegerProperty();
 		this.phaseNumber = new SimpleIntegerProperty();
-		this.pairingMethod = new ObjectProperty<PairingStrategy>() {
-
-			@Override
-			public void bind(ObservableValue<? extends PairingStrategy> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public boolean isBound() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public void unbind() {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public Object getBean() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public String getName() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void addListener(ChangeListener<? super PairingStrategy> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void removeListener(
-					ChangeListener<? super PairingStrategy> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void addListener(InvalidationListener arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void removeListener(InvalidationListener arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public PairingStrategy get() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void set(PairingStrategy arg0) {
-				// TODO Auto-generated method stub
-
-			}
-		};
-		this.roundDuration = new ObjectProperty<Duration>() {
-
-			@Override
-			public void bind(ObservableValue<? extends Duration> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public boolean isBound() {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			@Override
-			public void unbind() {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public Object getBean() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public String getName() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void addListener(ChangeListener<? super Duration> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void removeListener(ChangeListener<? super Duration> arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void addListener(InvalidationListener arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public void removeListener(InvalidationListener arg0) {
-				// TODO Auto-generated method stub
-
-			}
-
-			@Override
-			public Duration get() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			@Override
-			public void set(Duration arg0) {
-				// TODO Auto-generated method stub
-
-			}
-		};
+		this.pairingMethod = new SimpleObjectProperty<PairingStrategy>();
+		this.roundDuration = new SimpleObjectProperty<Duration>();
 	}
 }
