0ccea6453a2c58cefc905533e843dfc9ba6da04c
Jonas Auer <jonas.auer@gmx.de>
2015-01-08 21:19:48 +0100

    Added two more event phase views and added animation support for the EventPhaseView

--- Application/src/usspg31/tourney/controller/controls/EventPhaseViewController.java
+++ Application/src/usspg31/tourney/controller/controls/EventPhaseViewController.java
@@ -2,12 +2,19 @@ package usspg31.tourney.controller.controls;
 
 import java.io.IOException;
 
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 import usspg31.tourney.controller.MainWindow;
 import usspg31.tourney.controller.controls.eventphases.EventSetupPhaseController;
 import usspg31.tourney.controller.controls.eventphases.PreRegistrationPhaseController;
@@ -48,16 +55,38 @@ public class EventPhaseViewController {
 	private Node tournamentExecutionPhase;
 	private TournamentExecutionPhaseController tournamentExecutionPhaseController;
 
+	// Animations
+	private DoubleProperty phasePosition;
+	private Timeline currentAnimation;
+
+	private static final Duration transitionDuration = Duration.millis(300);
+	private static Interpolator transitionInterpolator = Interpolator.SPLINE(.4, 0, 0, 1);
 
 	@FXML private void initialize() throws IOException {
 		this.buttonClose.setOnAction(event -> {
 			MainWindow.getInstance().displayMainMenu();
 		});
 
+		this.phasePosition = new SimpleDoubleProperty(0);
+
 		this.loadSubViews();
 		this.initBreadcrumbs();
 
-		this.eventPhaseContainer.getChildren().addAll(this.eventSetupPhase);
+		this.breadcrumbEventSetup.setOnAction(event -> {
+			this.slideToPhase(0);
+		});
+		this.breadcrumbPreRegistration.setOnAction(event -> {
+			this.slideToPhase(1);
+		});
+		this.breadcrumbRegistration.setOnAction(event -> {
+			this.slideToPhase(2);
+		});
+		this.breadcrumbTournamentExecution.setOnAction(event -> {
+			this.slideToPhase(3);
+		});
+
+		this.eventPhaseContainer.getChildren().addAll(this.eventSetupPhase,
+				this.preRegistrationPhase, this.registrationPhase);
 	}
 
 	private void initBreadcrumbs() {
@@ -80,6 +109,45 @@ public class EventPhaseViewController {
 				.getResource("/ui/fxml/controls/eventphases/event-setup-phase.fxml"));
 		this.eventSetupPhase = eventSetupPhaseLoader.load();
 		//this.eventSetupPhaseController = eventSetupPhaseLoader.getController();
+		this.eventSetupPhase.translateXProperty().bind(
+				this.eventPhaseContainer.widthProperty().multiply(0)
+				.subtract(this.eventPhaseContainer.widthProperty()
+						.multiply(this.phasePosition)));
 		this.eventSetupPhase.setVisible(true);
+
+		FXMLLoader preRegistrationPhaseLoader = new FXMLLoader(this.getClass()
+				.getResource("/ui/fxml/controls/eventphases/pre-registration-phase.fxml"));
+		this.preRegistrationPhase = preRegistrationPhaseLoader.load();
+		this.preRegistrationPhase.translateXProperty().bind(
+				this.eventPhaseContainer.widthProperty().multiply(1)
+				.subtract(this.eventPhaseContainer.widthProperty()
+						.multiply(this.phasePosition)));
+		this.preRegistrationPhase.setVisible(true);
+
+		FXMLLoader registrationPhaseLoader = new FXMLLoader(this.getClass()
+				.getResource("/ui/fxml/controls/eventphases/registration-phase.fxml"));
+		this.registrationPhase = registrationPhaseLoader.load();
+		this.registrationPhase.translateXProperty().bind(
+				this.eventPhaseContainer.widthProperty().multiply(2)
+				.subtract(this.eventPhaseContainer.widthProperty()
+						.multiply(this.phasePosition)));
+		this.registrationPhase.setVisible(true);
+
+		//		FXMLLoader tournamentExecutionPhaseLoader = new FXMLLoader(this.getClass()
+		//				.getResource("/ui/fxml/controls/eventphases/tournament-execution-phase.fxml"));
+		//		this.tournamentExecutionPhase = tournamentExecutionPhaseLoader.load();
+		//		this.tournamentExecutionPhase.setVisible(true);
+	}
+
+	private void slideToPhase(int phaseNumber) {
+		if (this.currentAnimation != null) {
+			this.currentAnimation.stop();
+		}
+		this.currentAnimation = new Timeline(
+				new KeyFrame(Duration.ZERO,
+						new KeyValue(this.phasePosition, this.phasePosition.get())),
+						new KeyFrame(transitionDuration,
+								new KeyValue(this.phasePosition, phaseNumber, transitionInterpolator)));
+		this.currentAnimation.play();
 	}
 }
