9326743e592452de079064619ebd7447c4cc5f48
Jonas Auer <jonas.auer@gmx.de>
2015-01-09 14:23:55 +0100

    Added interfaces providing methods for loading and unloading events and tournaments

---
+++ Application/src/usspg31/tourney/controller/controls/EventUser.java
@@ -0,0 +1,8 @@
+package usspg31.tourney.controller.controls;
+
+import usspg31.tourney.model.Event;
+
+public interface EventUser {
+	public void loadEvent(Event event);
+	public void unloadEvent();
+}
---
+++ Application/src/usspg31/tourney/controller/controls/TournamentUser.java
@@ -0,0 +1,8 @@
+package usspg31.tourney.controller.controls;
+
+import usspg31.tourney.model.Tournament;
+
+public interface TournamentUser {
+	public void loadTournament(Tournament tournament);
+	public void unloadTournament();
+}
