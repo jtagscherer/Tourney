c5186b363645c366c91f0581494b5eea872980ec
Fabian Biester <fabian@familie-biester.de>
2015-01-09 19:23:40 +0100

    added some methods for the single elimantion pairing strategy. also finished the elimination pairing strategy (there is no seeding implemen

--- Application/src/usspg31/tourney/model/PlayerScore.java
+++ Application/src/usspg31/tourney/model/PlayerScore.java
@@ -1,9 +1,11 @@
 package usspg31.tourney.model;
 
+import java.util.Comparator;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class PlayerScore {
+public class PlayerScore implements Comparable<PlayerScore> {
 
 	private Player player;
 	private final ObservableList<Integer> score;
@@ -24,4 +26,32 @@ public class PlayerScore {
 	public ObservableList<Integer> getScore() {
 		return this.score;
 	}
+
+	@Override
+	public int compareTo(PlayerScore o) {
+		// TODO Auto-generated method stub
+		if (this.getScore().get(0) > o.getScore().get(0)) {
+			return 1;
+		} else if (this.getScore().get(0) == (o.getScore().get(0))) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+
+	public class PlayerScoreComparator implements Comparator<PlayerScore> {
+
+		@Override
+		public int compare(PlayerScore o1, PlayerScore o2) {
+			// TODO Auto-generated method stub
+			if (o1.getScore().get(0) > o2.getScore().get(0)) {
+				return 1;
+			} else if (o1.getScore().get(0) == o2.getScore().get(0)) {
+				return 0;
+			} else {
+				return -1;
+			}
+		}
+	}
+
 }
