7768a59cce57afe62eb7e6f41059a6105f4ebec7
Fabian Biester <fabian@familie-biester.de>
2015-01-09 19:23:40 +0100

    added some methods for the single elimantion pairing strategy. also finished the elimination pairing strategy (there is no seeding implemen

--- Application/src/usspg31/tourney/model/pairingstrategies/FreeForAll.java
+++ Application/src/usspg31/tourney/model/pairingstrategies/FreeForAll.java
@@ -4,42 +4,46 @@ import java.util.ArrayList;
 import java.util.Random;
 
 import usspg31.tourney.model.Pairing;
-import usspg31.tourney.model.Player;
 import usspg31.tourney.model.PairingHelper;
+import usspg31.tourney.model.Player;
 import usspg31.tourney.model.Tournament;
 
 public class FreeForAll implements PairingStrategy {
 
 	@Override
-	public ArrayList<Pairing> generatePairing(Tournament value) {
+	public ArrayList<Pairing> generatePairing(Tournament tournament) {
 		ArrayList<Pairing> result = new ArrayList<>();
 		Random randomGenerator = new Random();
 		ArrayList<Player> randomList = new ArrayList<>();
 		Pairing partResult;
-		randomList.addAll(value.getRegisteredPlayers());
+		randomList.addAll(tournament.getRegisteredPlayers());
 		int randomNumber;
-		if (value.getRounds().size() == 0) {
+		if (tournament.getRounds().size() == 0) {
 			while (randomList.size() >= PairingHelper.findPhase(
-					value.getRounds().size(), value).getNumberOfOpponents()) {
+					tournament.getRounds().size(), tournament)
+					.getNumberOfOpponents()) {
 				partResult = new Pairing();
 
 				for (int i = 0; i < PairingHelper.findPhase(
-						value.getRounds().size(), value).getNumberOfOpponents(); i++) {
+						tournament.getRounds().size(), tournament)
+						.getNumberOfOpponents(); i++) {
 					randomNumber = randomGenerator.nextInt(randomList.size());
 					partResult.getOpponents().add(randomList.get(randomNumber));
 					randomList.remove(randomNumber);
 				}
 				result.add(partResult);
 			}
-		} else if (value.getRuleSet().getPhaseList()
-				.get(value.getRounds().size() - 1).getPairingMethod()
+		} else if (tournament.getRuleSet().getPhaseList()
+				.get(tournament.getRounds().size() - 1).getPairingMethod()
 				.getClass() != FreeForAll.class) {
 			while (randomList.size() >= PairingHelper.findPhase(
-					value.getRounds().size(), value).getNumberOfOpponents()) {
+					tournament.getRounds().size(), tournament)
+					.getNumberOfOpponents()) {
 				partResult = new Pairing();
 
 				for (int i = 0; i < PairingHelper.findPhase(
-						value.getRounds().size(), value).getNumberOfOpponents(); i++) {
+						tournament.getRounds().size(), tournament)
+						.getNumberOfOpponents(); i++) {
 					randomNumber = randomGenerator.nextInt(randomList.size());
 					partResult.getOpponents().add(randomList.get(randomNumber));
 					randomList.remove(randomNumber);
@@ -48,11 +52,13 @@ public class FreeForAll implements PairingStrategy {
 			}
 		} else {
 			while (randomList.size() >= PairingHelper.findPhase(
-					value.getRounds().size(), value).getNumberOfOpponents()) {
+					tournament.getRounds().size(), tournament)
+					.getNumberOfOpponents()) {
 				partResult = new Pairing();
 
 				for (int i = 0; i < PairingHelper.findPhase(
-						value.getRounds().size(), value).getNumberOfOpponents(); i++) {
+						tournament.getRounds().size(), tournament)
+						.getNumberOfOpponents(); i++) {
 					randomNumber = randomGenerator.nextInt(randomList.size());
 					partResult.getOpponents().add(randomList.get(randomNumber));
 					randomList.remove(randomNumber);
