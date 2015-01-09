fa929a0d601b8933627d44ba821ef5f375f28ae1
Jonas Auer <jonas.auer@gmx.de>
2015-01-09 14:24:51 +0100

    Tournament Phases Views for phases 2 to 4 are now implementing the previously added interfaces

--- Application/src/usspg31/tourney/controller/controls/eventphases/RegistrationPhaseController.java
+++ Application/src/usspg31/tourney/controller/controls/eventphases/RegistrationPhaseController.java
@@ -1,5 +1,20 @@
 package usspg31.tourney.controller.controls.eventphases;
 
-public class RegistrationPhaseController {
+import usspg31.tourney.controller.controls.TournamentUser;
+import usspg31.tourney.model.Tournament;
+
+public class RegistrationPhaseController implements TournamentUser {
+
+	@Override
+	public void loadTournament(Tournament tournament) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unloadTournament() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
