cc73732e58eeb8fdd5736912b62e0e55692d20d8
Jonas Auer <jonas.auer@gmx.de>
2015-01-10 18:47:11 +0100

    Refactored all remaining lambdas bound to onAction events into seperate methods

--- Application/src/usspg31/tourney/controller/MainMenuController.java
+++ Application/src/usspg31/tourney/controller/MainMenuController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
@@ -34,81 +35,81 @@ public class MainMenuController {
 
 	@FXML
 	private void initialize() {
-		this.buttonNewEvent.setOnAction(event -> {
-			// create a new event and open it
-				log.finer("New Event Button was clicked");
-				MainWindow.getInstance().getEventPhaseViewController()
-						.loadEvent(new Event());
-				MainWindow.getInstance().slideUp(
-						MainWindow.getInstance().getEventPhaseView());
-			});
-		this.buttonOpenEvent
-				.setOnAction(event -> {
-					// open the file chooser dialog and load the chosen event
-					log.finer("Open Event Button was clicked");
-					FileChooser fileChooser = new FileChooser();
-					fileChooser.setTitle("Eventdatei öffnen");
-					fileChooser.getExtensionFilters().add(
-							new ExtensionFilter("Tourney Eventdateien (*.tef)",
-									"*.tef"));
-					File selectedFile = fileChooser.showOpenDialog(EntryPoint
-							.getPrimaryStage());
-					if (selectedFile != null) {
-						Event loadedEvent = null;
 
-						try {
-							loadedEvent = FileLoader
-									.loadEventFromFile(selectedFile
-											.getAbsolutePath());
-						} catch (Exception e) {
-							log.log(Level.SEVERE,
-									"Could not load the specified event.", e);
-							Dialogs.create()
-									.owner(EntryPoint.getPrimaryStage())
-									.title("Fehler")
-									.message(
-											"Die Eventdatei \""
-													+ selectedFile.getName()
-													+ "\" konnte nicht geladen werden.\nBitte stellen Sie sicher, dass es sich dabei um eine gültige Eventdatei handelt.")
-									.showError();
-						}
-
-						if (loadedEvent != null) {
-							EventPhaseViewController eventPhaseViewController = MainWindow
-									.getInstance()
-									.getEventPhaseViewController();
-
-							eventPhaseViewController.loadEvent(loadedEvent);
-							eventPhaseViewController
-									.setLoadedEventFile(selectedFile);
-
-							MainWindow.getInstance().slideUp(
-									MainWindow.getInstance()
-											.getEventPhaseView());
-						}
-					}
-				});
-
-		this.buttonOpenOptions
-				.setOnAction(event -> {
-					log.finer("Options Button was clicked");
-
-					MainWindow
-							.getInstance()
-							.getOptionsViewController()
-							.setExitProperties(
-									"Hauptmenü",
-									"Zurückkehren",
-									"Kehren Sie zum Hauptmenü zurück",
-									() -> {
-										MainWindow.getInstance().slideDown(
-												MainWindow.getInstance()
-														.getMainMenu());
-									});
-
-					MainWindow.getInstance().slideUp(
-							MainWindow.getInstance().getOptionsView());
-				});
 	}
 
+	@FXML private void onButtonNewEventClicked(ActionEvent event) {
+		// create a new event and open it
+		log.finer("New Event Button was clicked");
+		MainWindow.getInstance().getEventPhaseViewController()
+		.loadEvent(new Event());
+		MainWindow.getInstance().slideUp(
+				MainWindow.getInstance().getEventPhaseView());
+	}
+
+	@FXML private void onButtonOpenEventClicked(ActionEvent event) {
+		// open the file chooser dialog and load the chosen event
+		log.finer("Open Event Button was clicked");
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Eventdatei öffnen");
+		fileChooser.getExtensionFilters().add(
+				new ExtensionFilter("Tourney Eventdateien (*.tef)",
+						"*.tef"));
+		File selectedFile = fileChooser.showOpenDialog(EntryPoint
+				.getPrimaryStage());
+		if (selectedFile != null) {
+			Event loadedEvent = null;
+
+			try {
+				loadedEvent = FileLoader
+						.loadEventFromFile(selectedFile
+								.getAbsolutePath());
+			} catch (Exception e) {
+				log.log(Level.SEVERE,
+						"Could not load the specified event.", e);
+				Dialogs.create()
+				.owner(EntryPoint.getPrimaryStage())
+				.title("Fehler")
+				.message(
+						"Die Eventdatei \""
+								+ selectedFile.getName()
+								+ "\" konnte nicht geladen werden.\nBitte stellen Sie sicher, dass es sich dabei um eine gültige Eventdatei handelt.")
+								.showError();
+			}
+
+			if (loadedEvent != null) {
+				EventPhaseViewController eventPhaseViewController = MainWindow
+						.getInstance()
+						.getEventPhaseViewController();
+
+				eventPhaseViewController.loadEvent(loadedEvent);
+				eventPhaseViewController
+				.setLoadedEventFile(selectedFile);
+
+				MainWindow.getInstance().slideUp(
+						MainWindow.getInstance()
+						.getEventPhaseView());
+			}
+		}
+	}
+
+	@FXML private void onButtonOpenTournamentModuleEditorClicked(ActionEvent event) {
+
+	}
+
+	@FXML private void onButtonOpenOptionsClicked(ActionEvent event) {
+		log.finer("Options Button was clicked");
+
+		MainWindow.getInstance().getOptionsViewController().setExitProperties(
+				"Hauptmenü",
+				"Zurückkehren",
+				"Kehren Sie zum Hauptmenü zurück",
+				() -> {
+					MainWindow.getInstance().slideDown(
+							MainWindow.getInstance()
+							.getMainMenu());
+				});
+
+		MainWindow.getInstance().slideUp(MainWindow.getInstance().getOptionsView());
+	}
 }
