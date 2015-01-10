1effedf10ac10d668af28649d6827646d9956557
Jonas Auer <jonas.auer@gmx.de>
2015-01-11 00:35:03 +0100

    Added a new interface to the modal dialog api Fixed NullPointerException in the ModalDialog

---
+++ Application/src/usspg31/tourney/controller/dialogs/IModalDialogProvider.java
@@ -0,0 +1,13 @@
+package usspg31.tourney.controller.dialogs;
+
+public interface IModalDialogProvider<P, R> extends DialogContent<P, R> {
+
+	void initModalDialog(ModalDialog<P, R> modalDialog);
+
+	public default ModalDialog<P, R> modalDialog() {
+		ModalDialog<P, R> modalDialog = new ModalDialog<>(this);
+		this.initModalDialog(modalDialog);
+		return modalDialog;
+	}
+
+}
