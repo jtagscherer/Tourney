652d99bdc7fccbf1d07ac2854c7402b77097dad8
Jonas Auer <jonas.auer@gmx.de>
2015-01-11 00:36:04 +0100

    Added default implementation in interface DialogContent

--- Application/src/usspg31/tourney/controller/dialogs/DialogContent.java
+++ Application/src/usspg31/tourney/controller/dialogs/DialogContent.java
@@ -5,7 +5,7 @@ import javafx.scene.Node;
 public interface DialogContent<P, R> {
 
 	default void setDialogRoot(ModalDialog<P, R> dialogRoot) { }
-	Node getRoot();
+	default Node getRoot() { return (Node) this; }
 	default void setProperties(P properties) { }
 	default R getReturnValue() { return null; }
 
