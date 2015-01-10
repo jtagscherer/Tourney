c3104c9cca3d240ac2a7d51e88711e25219fe2f1
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-01-10 20:08:50 +0100

    Moved the tests to a new folder

---
+++ Application/test/usspg31/tourney/tests/model/filemanagement/TestFileSaver.java
@@ -0,0 +1,20 @@
+package usspg31.tourney.tests.model.filemanagement;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestFileSaver {
+
+	@Before
+	public void init() {
+
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
