16132975b83a6ba5d6ba3d2d93dfb0e1e8428799
Fabian Biester <fabian@familie-biester.de>
2015-01-11 13:49:17 +0100

    Finished the unit test for the swiss system pairing strategy

--- Application/test/usspg31/tourney/tests/pairingstrategies/TestPairingStrategies.java
+++ Application/test/usspg31/tourney/tests/pairingstrategies/TestPairingStrategies.java
@@ -1,5 +1,7 @@
 package usspg31.tourney.tests.pairingstrategies;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 
 import org.junit.Test;
@@ -12,11 +14,12 @@ import usspg31.tourney.model.RoundGeneratorFactory;
 import usspg31.tourney.model.Tournament;
 import usspg31.tourney.model.TournamentModule;
 import usspg31.tourney.model.pairingstrategies.SingleElimination;
+import usspg31.tourney.model.pairingstrategies.SwissSystem;
 
 public class TestPairingStrategies {
 
 	@Test
-	public void testSingleEliminationFirstRoundInTournament() {
+	public void testSingleElimination() {
 		Tournament testTournament = new Tournament();
 		TournamentModule testTournamentMoudle = new TournamentModule();
 		GamePhase testGamePhase = new GamePhase();
@@ -43,7 +46,7 @@ public class TestPairingStrategies {
 		testTournament.getRounds().add(
 				testRoundGenerator.generateRound(testTournament));
 
-		// assertEquals();
+		// the second player in each pairing wins
 		Integer[] prePlayerScore = { 0, 4 };
 		for (Pairing testPairing : testTournament.getRounds().get(0)
 				.getPairings()) {
@@ -53,6 +56,62 @@ public class TestPairingStrategies {
 		}
 		testTournament.getRounds().add(
 				testRoundGenerator.generateRound(testTournament));
+		//
+		assertEquals(testTournament.getRounds().get(0).getPairings().get(0)
+				.getOpponents().get(1), testTournament.getRounds().get(1)
+				.getPairings().get(0).getOpponents().get(0));
+
+	}
+
+	@Test
+	public void testSwissSystem() {
+		Tournament testTournament = new Tournament();
+		TournamentModule testTournamentMoudle = new TournamentModule();
+		GamePhase testGamePhase = new GamePhase();
+
+		testGamePhase.setNumberOfOpponents(2);
+		testGamePhase.setPhaseNumber(0);
+		testGamePhase.setRoundCount(10);
+		testGamePhase.setPairingMethod(new SwissSystem());
+
+		testTournamentMoudle.getPhaseList().add(testGamePhase);
+		testTournament.setRuleSet(testTournamentMoudle);
+
+		ArrayList<Player> testRemainingPlayer = new ArrayList<>();
+		Player testPlayer;
+		for (int i = 0; i < 8; i++) {
+			testPlayer = new Player();
+			testPlayer.setId(Integer.toString(i));
+
+			testRemainingPlayer.add(testPlayer);
+		}
+		testTournament.getRemainingPlayers().addAll(testRemainingPlayer);
+
+		RoundGeneratorFactory testRoundGenerator = new RoundGeneratorFactory();
+		testTournament.getRounds().add(
+				testRoundGenerator.generateRound(testTournament));
+		PlayerScore playerScore;
+
+		for (Player playlerForScore : testTournament.getRemainingPlayers()) {
+			playerScore = new PlayerScore();
+			playerScore.setPlayer(playlerForScore);
+			playerScore.getScore().add(
+					Integer.parseInt(playlerForScore.getId()) * 10);
+
+			testTournament.getScoreTable().add(playerScore);
+		}
+
+		testTournament.getRounds().add(
+				testRoundGenerator.generateRound(testTournament));
+		// Player with the highest id is first player added to the second round
+		assertEquals(testTournament.getRounds().get(1).getPairings().get(0)
+				.getOpponents().get(0), testTournament.getRemainingPlayers()
+				.get(testTournament.getRemainingPlayers().size() - 1));
+
+		// Player at second position have to play against him
+		assertEquals(testTournament.getRounds().get(1).getPairings().get(0)
+				.getOpponents().get(1), testTournament.getRemainingPlayers()
+				.get(testTournament.getRemainingPlayers().size() - 2));
 
 	}
 }
