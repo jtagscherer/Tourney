4dcbf6057bcd038cb1c66b831292a0eb5b63c095
Jonas Auer <jonas.auer@gmx.de>
2015-01-11 22:51:45 +0100

    Added functionality to the TournamentExecutionPhase

--- Application/src/usspg31/tourney/controller/controls/eventphases/TournamentExecutionPhaseController.java
+++ Application/src/usspg31/tourney/controller/controls/eventphases/TournamentExecutionPhaseController.java
@@ -1,20 +1,67 @@
 package usspg31.tourney.controller.controls.eventphases;
 
+import java.util.logging.Logger;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import usspg31.tourney.controller.controls.PairingView;
 import usspg31.tourney.controller.controls.TournamentUser;
+import usspg31.tourney.model.Pairing;
+import usspg31.tourney.model.PlayerScore;
+import usspg31.tourney.model.RoundGeneratorFactory;
 import usspg31.tourney.model.Tournament;
 
 public class TournamentExecutionPhaseController implements TournamentUser {
 
+	private static final Logger log = Logger
+			.getLogger(TournamentExecutionPhaseController.class.getName());
+
+	@FXML private PairingView pairingView;
+	@FXML private Button buttonStartRound;
+	@FXML private Button buttonEnterResult;
+
+	private Tournament loadedTournament;
+	private RoundGeneratorFactory roundGenerator = new RoundGeneratorFactory();
+
 	@Override
 	public void loadTournament(Tournament tournament) {
-		// TODO Auto-generated method stub
+		this.loadedTournament = tournament;
 
+		this.pairingView.loadTournament(tournament);
+
+		this.buttonEnterResult.disableProperty().bind(this.pairingView.selectedPairingProperty().isNull());
 	}
 
 	@Override
 	public void unloadTournament() {
-		// TODO Auto-generated method stub
-
+		this.pairingView.unloadTournament();
 	}
 
+	@FXML private void onButtonStartRoundClicked(ActionEvent event) {
+		this.loadedTournament.getRounds().add(
+				this.roundGenerator.generateRound(this.loadedTournament));
+		this.buttonStartRound.setDisable(true);
+	}
+
+	@FXML private void onButtonEnterResultClicked(ActionEvent event) {
+		// TODO: open dialog to enter score for every player
+
+		boolean roundFinished = true;
+		roundFinishCheck:
+			for (Pairing pairing : this.loadedTournament.getRounds().get(
+					this.loadedTournament.getRounds().size() - 1).getPairings()) {
+				for (PlayerScore playerScore : pairing.getScoreTable()) {
+					for (Integer score : playerScore.getScore()) {
+						if (score == null) {
+							roundFinished = false;
+							break roundFinishCheck;
+						}
+					}
+				}
+			}
+		if (roundFinished) {
+			this.buttonStartRound.setDisable(false);
+		}
+	}
 }
