8ce4cb191122ed3083760b160b2f846d67e3e687
Fabian Biester <fabian@familie-biester.de>
2015-01-14 23:15:03 +0100

    started with the double elimination/modified swiss system

--- Application/src/usspg31/tourney/model/pairingstrategies/ModifiedSwissSystem.java
+++ Application/src/usspg31/tourney/model/pairingstrategies/ModifiedSwissSystem.java
@@ -1,16 +1,60 @@
 package usspg31.tourney.model.pairingstrategies;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import usspg31.tourney.model.Pairing;
+import usspg31.tourney.model.PairingHelper;
+import usspg31.tourney.model.Player;
+import usspg31.tourney.model.PlayerScore;
 import usspg31.tourney.model.Tournament;
 
 public class ModifiedSwissSystem implements PairingStrategy {
 
 	@Override
 	public ArrayList<Pairing> generatePairing(Tournament tournament) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Pairing> result = new ArrayList<>();
+		ArrayList<PlayerScore> opponents = new ArrayList<>();
+
+		Pairing partResult;
+
+		Integer[] numberOfScores;
+		PlayerScore score;
+
+		ArrayList<ArrayList<Player>> test = new ArrayList<>();
+
+		if (tournament.getRounds().size() == 0) {
+
+			Random randomGenerator = new Random();
+			ArrayList<Player> randomList = new ArrayList<>();
+			randomList.addAll(tournament.getRemainingPlayers());
+			int randomNumber;
+
+			while (randomList.size() >= PairingHelper.findPhase(
+					tournament.getRounds().size(), tournament)
+					.getNumberOfOpponents()) {
+				partResult = new Pairing();
+				partResult.getScoreTable().addAll(opponents);
+
+				for (int i = 0; i < PairingHelper.findPhase(
+						tournament.getRounds().size(), tournament)
+						.getNumberOfOpponents(); i++) {
+					randomNumber = randomGenerator.nextInt(randomList.size());
+
+					score = new PlayerScore();
+					score.setPlayer(randomList.get(randomNumber));
+					numberOfScores = new Integer[tournament.getRuleSet()
+							.getPossibleScores().size()];
+					score.getScore().addAll(numberOfScores);
+					partResult.getScoreTable().add(score);
+
+					partResult.getOpponents().add(randomList.get(randomNumber));
+					randomList.remove(randomNumber);
+				}
+				result.add(partResult);
+			}
+		}
+		return result;
 	}
 
 	@Override
