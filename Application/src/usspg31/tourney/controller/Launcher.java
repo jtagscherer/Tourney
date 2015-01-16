c67d0246ea429800efb5865219e86f6afb5a6102
Jonas Auer <jonas.auer@gmx.de>
2015-01-16 14:24:27 +0100

    Added comments to the Launcher, made it log possibly thrown exceptions

--- Application/src/usspg31/tourney/controller/Launcher.java
+++ Application/src/usspg31/tourney/controller/Launcher.java
@@ -1,11 +1,24 @@
 package usspg31.tourney.controller;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class is only needed to set some VM arguments prior to executing the
+ * actual application.
+ */
 public class Launcher {
 
-	public static void main(String[] args) {
-		// Force the usage of GPU acceleration prior to launching the application
-		System.setProperty("prism.forceGPU", "true");
-		EntryPoint.main(args);
-	}
+	private static final Logger log = Logger.getLogger(Launcher.class
+			.getName());
 
+	public static void main(String[] args) {
+		try {
+			// Force the usage of GPU acceleration prior to launching the application
+			System.setProperty("prism.forceGPU", "true");
+			EntryPoint.main(args);
+		} catch (Throwable t) {
+			log.log(Level.SEVERE, t.getMessage(), t);
+		}
+	}
 }
