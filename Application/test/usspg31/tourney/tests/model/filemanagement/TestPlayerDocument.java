fb1be2c56ba2e77fc650d5e4ec8ef209aeb89086
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-01-17 14:00:14 +0100

    Fixed the tests after the refactorings

--- Application/test/usspg31/tourney/tests/model/filemanagement/TestPlayerDocument.java
+++ Application/test/usspg31/tourney/tests/model/filemanagement/TestPlayerDocument.java
@@ -18,55 +18,55 @@ import usspg31.tourney.model.Player;
 import usspg31.tourney.model.filemanagement.PlayerDocument;
 
 public class TestPlayerDocument {
-	private PlayerDocument document;
+    private PlayerDocument document;
 
-	@Before
-	public void init() {
-		DocumentBuilderFactory documentFactory = DocumentBuilderFactory
-				.newInstance();
-		DocumentBuilder documentBuilder = null;
-		try {
-			documentBuilder = documentFactory.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-		this.document = new PlayerDocument(documentBuilder.newDocument());
+    @Before
+    public void init() {
+	DocumentBuilderFactory documentFactory = DocumentBuilderFactory
+		.newInstance();
+	DocumentBuilder documentBuilder = null;
+	try {
+	    documentBuilder = documentFactory.newDocumentBuilder();
+	} catch (ParserConfigurationException e) {
+	    e.printStackTrace();
 	}
+	this.document = new PlayerDocument(documentBuilder.newDocument());
+    }
 
-	@Test
-	public void testPlayerList() {
-		ObservableList<Player> playerList = FXCollections.observableArrayList();
+    @Test
+    public void testPlayerList() {
+	ObservableList<Player> playerList = FXCollections.observableArrayList();
 
-		// Add a player to the list
-		Player player = new Player();
-		player.setFirstName("Peter");
-		player.setLastName("Player");
-		player.setId("2");
-		player.setMailAdress("p.player@mail.com");
-		player.setNickName("pplayer");
-		player.setStartingNumber("3");
-		player.setPayed(true);
-		player.setDisqualified(false);
+	// Add a player to the list
+	Player player = new Player();
+	player.setFirstName("Peter");
+	player.setLastName("Player");
+	player.setId("2");
+	player.setMailAdress("p.player@mail.com");
+	player.setNickName("pplayer");
+	player.setStartingNumber("3");
+	player.setPayed(true);
+	player.setDisqualified(false);
 
-		playerList.add(player);
+	playerList.add(player);
 
-		// Write the data to the document
-		this.document.appendPlayerList(playerList);
+	// Write the data to the document
+	this.document.appendPlayerList(playerList);
 
-		// Read the data back from the document
-		ArrayList<Player> readPlayers = this.document.getPlayerList();
+	// Read the data back from the document
+	ArrayList<Player> readPlayers = this.document.getPlayerList();
 
-		// Test the data
-		assertEquals(1, readPlayers.size());
+	// Test the data
+	assertEquals(1, readPlayers.size());
 
-		Player readPlayer = readPlayers.get(0);
-		assertEquals("Peter", readPlayer.getFirstName());
-		assertEquals("Player", readPlayer.getLastName());
-		assertEquals("p.player@mail.com", readPlayer.getMailAddress());
-		assertEquals("pplayer", readPlayer.getNickName());
-		assertEquals("3", readPlayer.getStartingNumber());
-		assertEquals(true, readPlayer.getPayed());
-		assertEquals(false, readPlayer.getDisqualified());
-	}
+	Player readPlayer = readPlayers.get(0);
+	assertEquals("Peter", readPlayer.getFirstName());
+	assertEquals("Player", readPlayer.getLastName());
+	assertEquals("p.player@mail.com", readPlayer.getMailAddress());
+	assertEquals("pplayer", readPlayer.getNickName());
+	assertEquals("3", readPlayer.getStartingNumber());
+	assertEquals(true, readPlayer.hasPayed());
+	assertEquals(false, readPlayer.isDisqualified());
+    }
 
 }
--- Application/test/usspg31/tourney/tests/model/filemanagement/TestTournamentDocument.java
+++ Application/test/usspg31/tourney/tests/model/filemanagement/TestTournamentDocument.java
@@ -29,234 +29,234 @@ import usspg31.tourney.model.filemanagement.TournamentDocument;
 import usspg31.tourney.model.pairingstrategies.SwissSystem;
 
 public class TestTournamentDocument {
-	private TournamentDocument document;
+    private TournamentDocument document;
 
-	@Before
-	public void init() {
-		DocumentBuilderFactory documentFactory = DocumentBuilderFactory
-				.newInstance();
-		DocumentBuilder documentBuilder = null;
-		try {
-			documentBuilder = documentFactory.newDocumentBuilder();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		}
-		this.document = new TournamentDocument(documentBuilder.newDocument());
+    @Before
+    public void init() {
+	DocumentBuilderFactory documentFactory = DocumentBuilderFactory
+		.newInstance();
+	DocumentBuilder documentBuilder = null;
+	try {
+	    documentBuilder = documentFactory.newDocumentBuilder();
+	} catch (ParserConfigurationException e) {
+	    e.printStackTrace();
 	}
+	this.document = new TournamentDocument(documentBuilder.newDocument());
+    }
 
-	@Test
-	public void testMetaData() {
-		Tournament tournament = new Tournament();
-		tournament.setName("TestTournament");
+    @Test
+    public void testMetaData() {
+	Tournament tournament = new Tournament();
+	tournament.setName("TestTournament");
 
-		this.document.appendMetaData(tournament);
+	this.document.appendMetaData(tournament);
 
-		assertEquals("TestTournament", this.document.getTournamentName());
-	}
+	assertEquals("TestTournament", this.document.getTournamentName());
+    }
 
-	@Test
-	public void testAdministratorList() {
-		// Add a list of event administrators
-		ObservableList<TournamentAdministrator> tournamentAdministrators = FXCollections
-				.observableArrayList();
-		TournamentAdministrator administrator = new TournamentAdministrator();
-		administrator.setFirstName("Aaron");
-		administrator.setLastName("Admin");
-		administrator.setMailAdress("a.admin@mail.com");
-		administrator.setPhoneNumber("123456");
-		tournamentAdministrators.add(administrator);
+    @Test
+    public void testAdministratorList() {
+	// Add a list of event administrators
+	ObservableList<TournamentAdministrator> tournamentAdministrators = FXCollections
+		.observableArrayList();
+	TournamentAdministrator administrator = new TournamentAdministrator();
+	administrator.setFirstName("Aaron");
+	administrator.setLastName("Admin");
+	administrator.setMailAdress("a.admin@mail.com");
+	administrator.setPhoneNumber("123456");
+	tournamentAdministrators.add(administrator);
 
-		this.document.appendAdministratorList(tournamentAdministrators);
+	this.document.appendAdministratorList(tournamentAdministrators);
 
-		ArrayList<TournamentAdministrator> readAdministrators = this.document
-				.getTournamentAdministrators();
+	ArrayList<TournamentAdministrator> readAdministrators = this.document
+		.getTournamentAdministrators();
 
-		// Test the administrator list
-		assertEquals(1, readAdministrators.size());
-		assertEquals("Aaron", readAdministrators.get(0).getFirstName());
-		assertEquals("Admin", readAdministrators.get(0).getLastName());
-		assertEquals("a.admin@mail.com", readAdministrators.get(0)
-				.getMailAddress());
-		assertEquals("123456", readAdministrators.get(0).getPhoneNumber());
-	}
+	// Test the administrator list
+	assertEquals(1, readAdministrators.size());
+	assertEquals("Aaron", readAdministrators.get(0).getFirstName());
+	assertEquals("Admin", readAdministrators.get(0).getLastName());
+	assertEquals("a.admin@mail.com", readAdministrators.get(0)
+		.getMailAddress());
+	assertEquals("123456", readAdministrators.get(0).getPhoneNumber());
+    }
 
-	@Test
-	public void testPlayerList() {
-		ObservableList<Player> playerList = FXCollections.observableArrayList();
-		Player player = new Player();
-		player.setFirstName("Peter");
-		player.setLastName("Player");
-		player.setId("2");
-		player.setMailAdress("p.player@mail.com");
-		player.setNickName("pplayer");
-		player.setStartingNumber("3");
-		player.setPayed(true);
-		player.setDisqualified(false);
-		playerList.add(player);
+    @Test
+    public void testPlayerList() {
+	ObservableList<Player> playerList = FXCollections.observableArrayList();
+	Player player = new Player();
+	player.setFirstName("Peter");
+	player.setLastName("Player");
+	player.setId("2");
+	player.setMailAdress("p.player@mail.com");
+	player.setNickName("pplayer");
+	player.setStartingNumber("3");
+	player.setPayed(true);
+	player.setDisqualified(false);
+	playerList.add(player);
 
-		// Write the data to the document
-		this.document.appendPlayerList(playerList,
-				TournamentDocument.PlayerListType.ATTENDANT_PLAYERS);
-		this.document.appendPlayerList(playerList,
-				TournamentDocument.PlayerListType.REGISTERED_PLAYERS);
-		this.document.appendPlayerList(playerList,
-				TournamentDocument.PlayerListType.REMAINING_PLAYERS);
+	// Write the data to the document
+	this.document.appendPlayerList(playerList,
+		TournamentDocument.PlayerListType.ATTENDANT_PLAYERS);
+	this.document.appendPlayerList(playerList,
+		TournamentDocument.PlayerListType.REGISTERED_PLAYERS);
+	this.document.appendPlayerList(playerList,
+		TournamentDocument.PlayerListType.REMAINING_PLAYERS);
 
-		// Read the data from the document
-		ArrayList<Player> playerArrayList = new ArrayList<Player>(playerList);
+	// Read the data from the document
+	ArrayList<Player> playerArrayList = new ArrayList<Player>(playerList);
 
-		assertEquals(
-				1,
-				this.document.getPlayerList(
-						TournamentDocument.PlayerListType.ATTENDANT_PLAYERS,
-						playerArrayList).size());
-		assertEquals(
-				1,
-				this.document.getPlayerList(
-						TournamentDocument.PlayerListType.REMAINING_PLAYERS,
-						playerArrayList).size());
-		assertEquals(
-				1,
-				this.document.getPlayerList(
-						TournamentDocument.PlayerListType.REGISTERED_PLAYERS,
-						playerArrayList).size());
+	assertEquals(
+		1,
+		this.document.getPlayerList(
+			TournamentDocument.PlayerListType.ATTENDANT_PLAYERS,
+			playerArrayList).size());
+	assertEquals(
+		1,
+		this.document.getPlayerList(
+			TournamentDocument.PlayerListType.REMAINING_PLAYERS,
+			playerArrayList).size());
+	assertEquals(
+		1,
+		this.document.getPlayerList(
+			TournamentDocument.PlayerListType.REGISTERED_PLAYERS,
+			playerArrayList).size());
 
-		assertEquals(
-				"2",
-				this.document
-						.getPlayerList(
-								TournamentDocument.PlayerListType.ATTENDANT_PLAYERS,
-								playerArrayList).get(0).getId());
-		assertEquals(
-				"2",
-				this.document
-						.getPlayerList(
-								TournamentDocument.PlayerListType.REMAINING_PLAYERS,
-								playerArrayList).get(0).getId());
-		assertEquals(
-				"2",
-				this.document
-						.getPlayerList(
-								TournamentDocument.PlayerListType.REGISTERED_PLAYERS,
-								playerArrayList).get(0).getId());
-
-		Player readPlayer = this.document.getPlayerList(
+	assertEquals(
+		"2",
+		this.document
+			.getPlayerList(
 				TournamentDocument.PlayerListType.ATTENDANT_PLAYERS,
-				playerArrayList).get(0);
-		assertEquals("Peter", readPlayer.getFirstName());
-		assertEquals("Player", readPlayer.getLastName());
-		assertEquals("p.player@mail.com", readPlayer.getMailAddress());
-		assertEquals("pplayer", readPlayer.getNickName());
-		assertEquals("3", readPlayer.getStartingNumber());
-		assertEquals(true, readPlayer.getPayed());
-		assertEquals(false, readPlayer.getDisqualified());
-	}
+				playerArrayList).get(0).getId());
+	assertEquals(
+		"2",
+		this.document
+			.getPlayerList(
+				TournamentDocument.PlayerListType.REMAINING_PLAYERS,
+				playerArrayList).get(0).getId());
+	assertEquals(
+		"2",
+		this.document
+			.getPlayerList(
+				TournamentDocument.PlayerListType.REGISTERED_PLAYERS,
+				playerArrayList).get(0).getId());
 
-	@Test
-	public void testTournamentRounds() {
-		ObservableList<TournamentRound> tournamentRounds = FXCollections
-				.observableArrayList();
+	Player readPlayer = this.document.getPlayerList(
+		TournamentDocument.PlayerListType.ATTENDANT_PLAYERS,
+		playerArrayList).get(0);
+	assertEquals("Peter", readPlayer.getFirstName());
+	assertEquals("Player", readPlayer.getLastName());
+	assertEquals("p.player@mail.com", readPlayer.getMailAddress());
+	assertEquals("pplayer", readPlayer.getNickName());
+	assertEquals("3", readPlayer.getStartingNumber());
+	assertEquals(true, readPlayer.hasPayed());
+	assertEquals(false, readPlayer.isDisqualified());
+    }
 
-		// Create a player list
-		ArrayList<Player> playerList = new ArrayList<Player>();
-		Player player = new Player();
-		player.setFirstName("Peter");
-		player.setLastName("Player");
-		player.setId("2");
-		player.setMailAdress("p.player@mail.com");
-		player.setNickName("pplayer");
-		player.setStartingNumber("3");
-		player.setPayed(true);
-		player.setDisqualified(false);
-		playerList.add(player);
+    @Test
+    public void testTournamentRounds() {
+	ObservableList<TournamentRound> tournamentRounds = FXCollections
+		.observableArrayList();
 
-		// Add a new tournament round
-		TournamentRound round = new TournamentRound(1);
-		Pairing pairing = new Pairing();
-		pairing.getOpponents().add(player);
-		PlayerScore score = new PlayerScore();
-		score.setPlayer(player);
-		score.getScore().add(10);
-		pairing.getScoreTable().add(score);
-		round.getPairings().add(pairing);
-		tournamentRounds.add(round);
+	// Create a player list
+	ArrayList<Player> playerList = new ArrayList<Player>();
+	Player player = new Player();
+	player.setFirstName("Peter");
+	player.setLastName("Player");
+	player.setId("2");
+	player.setMailAdress("p.player@mail.com");
+	player.setNickName("pplayer");
+	player.setStartingNumber("3");
+	player.setPayed(true);
+	player.setDisqualified(false);
+	playerList.add(player);
 
-		this.document.appendTournamentRounds(tournamentRounds);
+	// Add a new tournament round
+	TournamentRound round = new TournamentRound(1);
+	Pairing pairing = new Pairing();
+	pairing.getOpponents().add(player);
+	PlayerScore score = new PlayerScore();
+	score.setPlayer(player);
+	score.getScore().add(10);
+	pairing.getScoreTable().add(score);
+	round.getPairings().add(pairing);
+	tournamentRounds.add(round);
 
-		ArrayList<TournamentRound> readTournamentRounds = this.document
-				.getTournamentRounds(playerList);
+	this.document.appendTournamentRounds(tournamentRounds);
 
-		assertEquals(1, readTournamentRounds.size());
+	ArrayList<TournamentRound> readTournamentRounds = this.document
+		.getTournamentRounds(playerList);
 
-		TournamentRound readRound = readTournamentRounds.get(0);
+	assertEquals(1, readTournamentRounds.size());
 
-		assertEquals(1, readRound.getRoundNumber());
+	TournamentRound readRound = readTournamentRounds.get(0);
 
-		assertEquals(1, readRound.getPairings().size());
+	assertEquals(1, readRound.getRoundNumber());
 
-		Pairing readPairing = readRound.getPairings().get(0);
-		assertEquals(1, readPairing.getOpponents().size());
+	assertEquals(1, readRound.getPairings().size());
 
-		Player readOpponent = readPairing.getOpponents().get(0);
-		assertEquals("Peter", readOpponent.getFirstName());
-		assertEquals("Player", readOpponent.getLastName());
-		assertEquals("p.player@mail.com", readOpponent.getMailAddress());
-		assertEquals("pplayer", readOpponent.getNickName());
-		assertEquals("3", readOpponent.getStartingNumber());
-		assertEquals("2", readOpponent.getId());
-		assertEquals(true, readOpponent.getPayed());
-		assertEquals(false, readOpponent.getDisqualified());
+	Pairing readPairing = readRound.getPairings().get(0);
+	assertEquals(1, readPairing.getOpponents().size());
 
-		assertEquals(1, readPairing.getScoreTable().size());
-		PlayerScore readScore = readPairing.getScoreTable().get(0);
+	Player readOpponent = readPairing.getOpponents().get(0);
+	assertEquals("Peter", readOpponent.getFirstName());
+	assertEquals("Player", readOpponent.getLastName());
+	assertEquals("p.player@mail.com", readOpponent.getMailAddress());
+	assertEquals("pplayer", readOpponent.getNickName());
+	assertEquals("3", readOpponent.getStartingNumber());
+	assertEquals("2", readOpponent.getId());
+	assertEquals(true, readOpponent.hasPayed());
+	assertEquals(false, readOpponent.isDisqualified());
 
-		assertEquals("2", readScore.getPlayer().getId());
+	assertEquals(1, readPairing.getScoreTable().size());
+	PlayerScore readScore = readPairing.getScoreTable().get(0);
 
-		assertEquals(1, readScore.getScore().size());
-		assertEquals(10, readScore.getScore().get(0).intValue());
-	}
+	assertEquals("2", readScore.getPlayer().getId());
 
-	@Test
-	public void testTournamentRules() {
-		// Create some new tournament rules
-		TournamentModule module = new TournamentModule();
-		PossibleScoring primaryScores = new PossibleScoring();
-		primaryScores.setPriorityValue(1);
-		primaryScores.getScores().put("Victory", 3);
-		primaryScores.getScores().put("Tie", 2);
-		primaryScores.getScores().put("Defeat", 1);
-		module.getPossibleScores().add(primaryScores);
-		GamePhase phase = new GamePhase();
-		phase.setPhaseNumber(1);
-		phase.setCutoff(16);
-		phase.setPairingMethod(new SwissSystem());
-		phase.setRoundCount(4);
-		phase.setRoundDuration(Duration.ofMinutes(10));
-		module.getPhaseList().add(phase);
+	assertEquals(1, readScore.getScore().size());
+	assertEquals(10, readScore.getScore().get(0).intValue());
+    }
 
-		// Write the rules to the document
-		this.document.appendTournamentRules(module);
+    @Test
+    public void testTournamentRules() {
+	// Create some new tournament rules
+	TournamentModule module = new TournamentModule();
+	PossibleScoring primaryScores = new PossibleScoring();
+	primaryScores.setPriority(1);
+	primaryScores.getScores().put("Victory", 3);
+	primaryScores.getScores().put("Tie", 2);
+	primaryScores.getScores().put("Defeat", 1);
+	module.getPossibleScores().add(primaryScores);
+	GamePhase phase = new GamePhase();
+	phase.setPhaseNumber(1);
+	phase.setCutoff(16);
+	phase.setPairingMethod(new SwissSystem());
+	phase.setRoundCount(4);
+	phase.setRoundDuration(Duration.ofMinutes(10));
+	module.getPhaseList().add(phase);
 
-		// Read the rules from the document
-		TournamentModule readTournamentModule = this.document
-				.getTournamentRules();
+	// Write the rules to the document
+	this.document.appendTournamentRules(module);
 
-		// Test the possible scorings in the rule set
-		assertEquals(1, readTournamentModule.getPossibleScores().size());
-		PossibleScoring readScoring = readTournamentModule.getPossibleScores()
-				.get(0);
-		assertEquals(1, readScoring.getPriority().get());
-		assertEquals(3, readScoring.getScores().get("Victory").intValue());
-		assertEquals(2, readScoring.getScores().get("Tie").intValue());
-		assertEquals(1, readScoring.getScores().get("Defeat").intValue());
+	// Read the rules from the document
+	TournamentModule readTournamentModule = this.document
+		.getTournamentRules();
 
-		// Test the game phases in the rule set
-		assertEquals(1, readTournamentModule.getPhaseList().size());
-		GamePhase readPhase = readTournamentModule.getPhaseList().get(0);
-		assertEquals(1, readPhase.getPhaseNumber());
-		assertEquals(16, readPhase.getCutoff());
-		assertTrue(readPhase.getPairingMethod() instanceof SwissSystem);
-		assertEquals(4, readPhase.getRoundCount());
-		assertEquals(Duration.ofMinutes(10), readPhase.getRoundDuration());
-	}
+	// Test the possible scorings in the rule set
+	assertEquals(1, readTournamentModule.getPossibleScores().size());
+	PossibleScoring readScoring = readTournamentModule.getPossibleScores()
+		.get(0);
+	assertEquals(1, readScoring.getPriority());
+	assertEquals(3, readScoring.getScores().get("Victory").intValue());
+	assertEquals(2, readScoring.getScores().get("Tie").intValue());
+	assertEquals(1, readScoring.getScores().get("Defeat").intValue());
+
+	// Test the game phases in the rule set
+	assertEquals(1, readTournamentModule.getPhaseList().size());
+	GamePhase readPhase = readTournamentModule.getPhaseList().get(0);
+	assertEquals(1, readPhase.getPhaseNumber());
+	assertEquals(16, readPhase.getCutoff());
+	assertTrue(readPhase.getPairingMethod() instanceof SwissSystem);
+	assertEquals(4, readPhase.getRoundCount());
+	assertEquals(Duration.ofMinutes(10), readPhase.getRoundDuration());
+    }
 }
