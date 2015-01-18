7155728d1ae9cf18fd85a16f6abe0995fb7a422a
Jonas Auer <jonas.auer@gmx.de>
2015-01-18 23:14:31 +0100

    AttendenceDialog should now be able to do what its there for

--- Application/src/usspg31/tourney/controller/dialogs/AttendenceDialog.java
+++ Application/src/usspg31/tourney/controller/dialogs/AttendenceDialog.java
@@ -1,13 +1,16 @@
 package usspg31.tourney.controller.dialogs;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import usspg31.tourney.controller.dialogs.modal.DialogButtons;
@@ -16,50 +19,103 @@ import usspg31.tourney.controller.dialogs.modal.IModalDialogProvider;
 import usspg31.tourney.controller.dialogs.modal.ModalDialog;
 import usspg31.tourney.model.Player;
 
-public class AttendenceDialog extends VBox implements IModalDialogProvider<List<Player>, List<Player>> {
+public class AttendenceDialog extends VBox implements
+IModalDialogProvider<ObservableList<Player>, ObservableList<Player>> {
 
-	private static final Logger log = Logger.getLogger(AttendenceDialog.class.getName());
+    private static final Logger log = Logger.getLogger(AttendenceDialog.class
+	    .getName());
 
-	@FXML private TableView<String> tableRegisteredPlayers;
-	@FXML private TableView<String> tableAttendingPlayers;
-	@FXML private Button buttonAddAttendee;
-	@FXML private Button buttonRemoveAttendee;
-	@FXML private Button buttonStartTournament;
+    @FXML
+    private TableView<Player> tableRegisteredPlayers;
+    @FXML
+    private TableView<Player> tableAttendingPlayers;
+    @FXML
+    private Button buttonAddAttendee;
+    @FXML
+    private Button buttonRemoveAttendee;
+    @FXML
+    private Button buttonStartTournament;
 
-	public AttendenceDialog() {
-		try {
-			FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/ui/fxml/dialogs/attendence-dialog.fxml"));
-			loader.setController(this);
-			loader.setRoot(this);
-			loader.load();
-		} catch (IOException e) {
-			log.log(Level.SEVERE, e.getMessage(), e);
-		}
+    public AttendenceDialog() {
+	try {
+	    FXMLLoader loader = new FXMLLoader(this.getClass().getResource(
+		    "/ui/fxml/dialogs/attendence-dialog.fxml"));
+	    loader.setController(this);
+	    loader.setRoot(this);
+	    loader.load();
+	} catch (IOException e) {
+	    log.log(Level.SEVERE, e.getMessage(), e);
 	}
+    }
 
-	@Override
-	public void setDialogRoot(ModalDialog<List<Player>, List<Player>> dialogRoot) {
-		this.buttonStartTournament.setOnAction(event -> {
-			// TODO: ask the user if he really intends to start the tournament
-			dialogRoot.exitWith(DialogResult.OK);
-		});
-	}
+    @FXML
+    public void initialize() {
+	TableColumn<Player, String> playerName = new TableColumn<>("Name");
+	playerName.setCellValueFactory(cellValue -> {
+	    return cellValue.getValue().firstNameProperty().concat(" ")
+		    .concat(cellValue.getValue().lastNameProperty());
+	});
+	this.tableAttendingPlayers.getColumns().add(playerName);
+	this.tableRegisteredPlayers.getColumns().add(playerName);
 
-	@Override
-	public void setProperties(List<Player> properties) {
-		// TODO: add the given list to the registered players table
-	}
+	this.buttonAddAttendee.disableProperty().bind(
+		this.tableRegisteredPlayers.getSelectionModel()
+			.selectedItemProperty().isNull());
+	this.buttonRemoveAttendee.disableProperty().bind(
+		this.tableAttendingPlayers.getSelectionModel()
+			.selectedItemProperty().isNull());
+    }
 
-	@Override
-	public List<Player> getReturnValue() {
-		// TODO: return the content of the attending players table
-		return null;
-	}
+    @Override
+    public void setDialogRoot(
+	    ModalDialog<ObservableList<Player>, ObservableList<Player>> dialogRoot) {
+	this.buttonStartTournament.setOnAction(event -> {
+	    // TODO: ask the user if he really intends to start the tournament
+	    dialogRoot.exitWith(DialogResult.OK);
+	});
+    }
 
-	@Override
-	public void initModalDialog(
-			ModalDialog<List<Player>, List<Player>> modalDialog) {
-		modalDialog.title("Teilnehmende Spieler").dialogButtons(
-				DialogButtons.NONE);
+    @Override
+    public void setProperties(ObservableList<Player> properties) {
+	this.tableRegisteredPlayers.setItems(FXCollections
+		.observableArrayList());
+	this.tableAttendingPlayers
+		.setItems(FXCollections.observableArrayList());
+
+	this.tableRegisteredPlayers.getItems().addAll(properties);
+    }
+
+    @Override
+    public ObservableList<Player> getReturnValue() {
+	return this.tableAttendingPlayers.getItems();
+    }
+
+    @Override
+    public void initModalDialog(
+	    ModalDialog<ObservableList<Player>, ObservableList<Player>> modalDialog) {
+	modalDialog.title("Teilnehmende Spieler").dialogButtons(
+		DialogButtons.NONE);
+    }
+
+    @FXML
+    private void onButtonAddAttendeeClicked(ActionEvent event) {
+	ObservableList<Player> selectedPlayers = this.tableRegisteredPlayers
+		.getSelectionModel().getSelectedItems();
+
+	for (Player p : selectedPlayers) {
+	    this.tableRegisteredPlayers.getItems().remove(p);
+	    this.tableAttendingPlayers.getItems().add(p);
 	}
+    }
+
+    @FXML
+    private void onButtonRemoveAttendeeClicked(ActionEvent event) {
+	ObservableList<Player> selectedPlayers = this.tableAttendingPlayers
+		.getSelectionModel().getSelectedItems();
+
+	for (Player p : selectedPlayers) {
+	    this.tableAttendingPlayers.getItems().remove(p);
+	    this.tableRegisteredPlayers.getItems().add(p);
+	}
+    }
 }
