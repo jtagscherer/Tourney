1c2de4af4028099796a3cf5a48f3f6f28e06179c
Jonas Auer <jonas.auer@gmx.de>
2015-01-18 16:03:53 +0100

    Replaced the complete Code of the EntryPoint with whatever was currently on the main branch, some commit of mine must have replaced the content of the file with an old one :/

--- Application/src/usspg31/tourney/controller/EntryPoint.java
+++ Application/src/usspg31/tourney/controller/EntryPoint.java
@@ -10,94 +10,96 @@ import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
-
-import org.controlsfx.control.action.Action;
-import org.controlsfx.dialog.Dialog;
-import org.controlsfx.dialog.Dialogs;
-
+import usspg31.tourney.controller.dialogs.modal.DialogButtons;
 import usspg31.tourney.controller.dialogs.modal.DialogResult;
+import usspg31.tourney.controller.dialogs.modal.SimpleDialog;
 import usspg31.tourney.model.undo.UndoManager;
 
-@SuppressWarnings("deprecation")
 public class EntryPoint extends Application {
-	private static final Logger log = Logger.getLogger(EntryPoint.class
-			.getName());
+    private static final Logger log = Logger.getLogger(EntryPoint.class
+	    .getName());
 
-	private static Stage primaryStage;
+    private static Stage primaryStage;
 
-	public static void main(String[] args) {
-		log.info("Starting Application");
-		log.info("Running JavaFX Version "
-				+ System.getProperty("javafx.runtime.version"));
+    public static void main(String[] args) {
+	log.info("Starting Application");
+	log.info("Running JavaFX Version "
+		+ System.getProperty("javafx.runtime.version"));
 
-		try {
-			launch(args);
-		} catch (Throwable t) {
-			log.log(Level.SEVERE, t.getMessage(), t);
-		}
+	try {
+	    launch(args);
+	} catch (Throwable t) {
+	    log.log(Level.SEVERE, t.getMessage(), t);
 	}
+    }
 
-	public static Stage getPrimaryStage() {
-		return primaryStage;
-	}
+    public static Stage getPrimaryStage() {
+	return primaryStage;
+    }
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		EntryPoint.primaryStage = primaryStage;
-		try {
-			Pane root = MainWindow.getInstance();
-			Scene scene = new Scene(root);
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Tourney");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+	EntryPoint.primaryStage = primaryStage;
+	try {
+	    Pane root = MainWindow.getInstance();
+	    Scene scene = new Scene(root);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("Tourney");
 
-			primaryStage.minWidthProperty().bind(root.minWidthProperty());
-			primaryStage.minHeightProperty().bind(root.minHeightProperty());
+	    primaryStage.minWidthProperty().bind(root.minWidthProperty());
+	    primaryStage.minHeightProperty().bind(root.minHeightProperty());
 
-			Platform.setImplicitExit(false);
+	    Platform.setImplicitExit(false);
 
-			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-				@Override
-				public void handle(WindowEvent event) {
-					event.consume();
-					UndoManager undoManager = MainWindow.getInstance()
-							.getEventPhaseViewController()
-							.getActiveUndoManager();
-					if (undoManager != null) {
-						if (undoManager.undoAvailable()) {
-							Action response = Dialogs
-									.create()
-									.owner(EntryPoint.getPrimaryStage())
-									.title("Warnung")
-									.message(
-											"Es sind ungesicherte Änderungen vorhanden.\n"
-													+ "Möchten Sie diese vor dem Beenden speichern?")
-													.actions(Dialog.ACTION_YES,
-															Dialog.ACTION_NO,
-															Dialog.ACTION_CANCEL).showWarning();
+	    primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+		@Override
+		public void handle(WindowEvent event) {
+		    event.consume();
+		    UndoManager undoManager = MainWindow.getInstance()
+			    .getEventPhaseViewController()
+			    .getActiveUndoManager();
+		    if (undoManager != null) {
+			if (undoManager.undoAvailable()) {
+			    new SimpleDialog<>(
+				    "Es sind ungesicherte Änderungen vorhanden.\n"
+					    + "Möchten Sie diese vor dem Beenden speichern?")
+				    .modalDialog()
+				    .title("Warnung")
+				    .dialogButtons(DialogButtons.YES_NO_CANCEL)
+				    .onResult(
+					    (result, returnValue) -> {
+						switch (result) {
+						case CANCEL:
+						    return;
+						case YES:
+						    DialogResult saveResponse = MainWindow
+							    .getInstance()
+							    .getEventPhaseViewController()
+							    .saveEvent();
 
-							if (response == Dialog.ACTION_CANCEL) {
-								return;
-							} else if (response == Dialog.ACTION_YES) {
-								DialogResult saveResponse = MainWindow.getInstance()
-										.getEventPhaseViewController()
-										.saveEvent();
+						    if (saveResponse != DialogResult.OK) {
+							return;
+						    }
 
-								if (saveResponse != DialogResult.OK) {
-									return;
-								}
-							}
+						    /*
+						     * Fall through if there was
+						     * no return yet
+						     */
+						default:
+						    primaryStage.close();
+						    Platform.exit();
+						    break;
 						}
-					}
-
-					primaryStage.close();
-					Platform.exit();
-				}
-			});
-
-			primaryStage.show();
-		} catch (Exception e) {
-			log.log(Level.SEVERE, e.getMessage(), e);
-			System.exit(1);
+					    }).show();
+			}
+		    }
 		}
+	    });
+
+	    primaryStage.show();
+	} catch (Exception e) {
+	    log.log(Level.SEVERE, e.getMessage(), e);
+	    System.exit(1);
 	}
+    }
 }
