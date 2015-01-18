fcf7fbe992cb2216de897a428fcd7ccc46d5b61a
Jonas Auer <jonas.auer@gmx.de>
2015-01-18 22:15:42 +0100

    TournamentExecutionPhaseController should now unload tournaments correctly

--- Application/src/usspg31/tourney/controller/controls/eventphases/TournamentExecutionPhaseController.java
+++ Application/src/usspg31/tourney/controller/controls/eventphases/TournamentExecutionPhaseController.java
@@ -14,54 +14,68 @@ import usspg31.tourney.model.Tournament;
 
 public class TournamentExecutionPhaseController implements TournamentUser {
 
-	private static final Logger log = Logger
-			.getLogger(TournamentExecutionPhaseController.class.getName());
+    private static final Logger log = Logger
+	    .getLogger(TournamentExecutionPhaseController.class.getName());
 
-	@FXML private PairingView pairingView;
-	@FXML private Button buttonStartRound;
-	@FXML private Button buttonEnterResult;
+    @FXML
+    private PairingView pairingView;
+    @FXML
+    private Button buttonStartRound;
+    @FXML
+    private Button buttonEnterResult;
 
-	private Tournament loadedTournament;
-	private RoundGeneratorFactory roundGenerator = new RoundGeneratorFactory();
+    private Tournament loadedTournament;
+    private RoundGeneratorFactory roundGenerator = new RoundGeneratorFactory();
 
-	@Override
-	public void loadTournament(Tournament tournament) {
-		this.loadedTournament = tournament;
+    @Override
+    public void loadTournament(Tournament tournament) {
+	this.loadedTournament = tournament;
 
-		this.pairingView.loadTournament(tournament);
+	this.pairingView.loadTournament(tournament);
 
-		this.buttonEnterResult.disableProperty().bind(this.pairingView.selectedPairingProperty().isNull());
+	this.buttonEnterResult.disableProperty().bind(
+		this.pairingView.selectedPairingProperty().isNull());
+
+	if (tournament.getRounds().size() == 0) {
+	    this.generateRound();
 	}
+    }
 
-	@Override
-	public void unloadTournament() {
-		this.pairingView.unloadTournament();
-	}
+    @Override
+    public void unloadTournament() {
+	this.pairingView.unloadTournament();
+    }
 
-	@FXML private void onButtonStartRoundClicked(ActionEvent event) {
-		this.loadedTournament.getRounds().add(
-				this.roundGenerator.generateRound(this.loadedTournament));
-		this.buttonStartRound.setDisable(true);
-	}
+    @FXML
+    private void onButtonStartRoundClicked(ActionEvent event) {
+	this.generateRound();
+    }
 
-	@FXML private void onButtonEnterResultClicked(ActionEvent event) {
-		// TODO: open dialog to enter score for every player
+    private void generateRound() {
+	this.loadedTournament.getRounds().add(
+		this.roundGenerator.generateRound(this.loadedTournament));
+	this.buttonStartRound.setDisable(true);
+    }
 
-		boolean roundFinished = true;
-		roundFinishCheck:
-			for (Pairing pairing : this.loadedTournament.getRounds().get(
-					this.loadedTournament.getRounds().size() - 1).getPairings()) {
-				for (PlayerScore playerScore : pairing.getScoreTable()) {
-					for (Integer score : playerScore.getScore()) {
-						if (score == null) {
-							roundFinished = false;
-							break roundFinishCheck;
-						}
-					}
-				}
-			}
-		if (roundFinished) {
-			this.buttonStartRound.setDisable(false);
+    @FXML
+    private void onButtonEnterResultClicked(ActionEvent event) {
+	// TODO: open dialog to enter score for every player
+
+	boolean roundFinished = true;
+	roundFinishCheck: for (Pairing pairing : this.loadedTournament
+		.getRounds().get(this.loadedTournament.getRounds().size() - 1)
+		.getPairings()) {
+	    for (PlayerScore playerScore : pairing.getScoreTable()) {
+		for (Integer score : playerScore.getScore()) {
+		    if (score == null) {
+			roundFinished = false;
+			break roundFinishCheck;
+		    }
 		}
+	    }
 	}
+	if (roundFinished) {
+	    this.buttonStartRound.setDisable(false);
+	}
+    }
 }
