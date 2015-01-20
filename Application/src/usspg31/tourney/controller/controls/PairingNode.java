5e94e3e71f41d6b451ee13098491996075136122
Jonas Auer <jonas.auer@gmx.de>
2015-01-20 22:08:41 +0100

    PairingView should now (theoretically) display all rounds and pairings once a tournament is loaded. Only testable if the tournament selection works.

---
+++ Application/src/usspg31/tourney/controller/controls/PairingNode.java
@@ -0,0 +1,109 @@
+package usspg31.tourney.controller.controls;
+
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import usspg31.tourney.model.Pairing;
+import usspg31.tourney.model.PlayerScore;
+import usspg31.tourney.model.PossibleScoring;
+import usspg31.tourney.model.Tournament;
+
+/**
+ * Control used to display a pairing within a tournament.
+ */
+public class PairingNode extends VBox {
+
+    private final Tournament tournament;
+    private final Pairing pairing;
+    private final int index;
+
+    private BooleanProperty selected;
+
+    private TableView<PlayerScore> opponentTable;
+
+    /**
+     * Initializes a new PairingNode.
+     *
+     * @param tournament the tournament to retrieve additional data from
+     * @param pairing the actual pairing to display
+     * @param index the id of the pairing
+     */
+    public PairingNode(Tournament tournament, Pairing pairing, int index) {
+        super(5);
+
+        this.tournament = tournament;
+        this.pairing = pairing;
+        this.index = index;
+
+        this.initContents();
+    }
+
+    /**
+     * Initializes the control's contents.
+     */
+    private void initContents() {
+        this.getChildren().add(new Label("#" + this.index));
+
+        this.opponentTable = new TableView<>();
+
+        TableColumn<PlayerScore, String> playerNameColumn = new TableColumn<>(
+                "Name");
+        playerNameColumn.setCellValueFactory(score -> {
+            return score.getValue().getPlayer().lastNameProperty();
+        });
+
+        this.opponentTable.getColumns().add(playerNameColumn);
+
+        for (PossibleScoring scoring : this.tournament.getRuleSet()
+                .getPossibleScores()) {
+            TableColumn<PlayerScore, String> scoreColumn = new TableColumn<>(
+                    Integer.toString(scoring.getPriority()));
+            scoreColumn.setCellValueFactory(score -> {
+                return new SimpleStringProperty(Integer.toString(score
+                        .getValue().getScore().get(scoring.getPriority())));
+            });
+        }
+
+        this.opponentTable.setItems(this.pairing.getScoreTable());
+    }
+
+    public Pairing getPairing() {
+        return this.pairing;
+    }
+
+    /**
+     * @return the selected property
+     */
+    public BooleanProperty selectedProperty() {
+        if (this.selected == null) {
+            this.selected = new SimpleBooleanProperty();
+        }
+        return this.selected;
+    }
+
+    /**
+     * @return the value of the selected property
+     */
+    public boolean isSelected() {
+        return this.selectedProperty().get();
+    }
+
+    /**
+     * @param value sets the new value for the selected property
+     */
+    public void setSelected(boolean value) {
+        this.selectedProperty().set(value);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        this.opponentTable.setItems(null);
+
+        super.finalize();
+    }
+
+}
--- Application/src/usspg31/tourney/controller/controls/PairingView.java
+++ Application/src/usspg31/tourney/controller/controls/PairingView.java
@@ -8,24 +8,17 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener.Change;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import usspg31.tourney.model.Pairing;
-import usspg31.tourney.model.PlayerScore;
-import usspg31.tourney.model.PossibleScoring;
 import usspg31.tourney.model.Tournament;
 import usspg31.tourney.model.TournamentRound;
 
@@ -34,194 +27,174 @@ import usspg31.tourney.model.TournamentRound;
  */
 public class PairingView extends VBox implements TournamentUser {
 
-    private static final Logger log = Logger.getLogger(PairingView.class
-	    .getName());
+    private static final Logger log = Logger.getLogger(PairingView.class.getName());
 
-    @FXML
-    private Button buttonScrollBreadcrumbsLeft;
-    @FXML
-    private Button buttonScrollBreadcrumbsRight;
-    @FXML
-    private ScrollPane breadcrumbScrollPane;
-    @FXML
-    private HBox breadcrumbContainer;
-    @FXML
-    private FlowPane pairingContainer;
+    @FXML private Button buttonScrollBreadcrumbsLeft;
+    @FXML private Button buttonScrollBreadcrumbsRight;
+    @FXML private ScrollPane breadcrumbScrollPane;
+    @FXML private HBox breadcrumbContainer;
+    @FXML private FlowPane pairingContainer;
 
     private Tournament loadedTournament;
 
     private IntegerProperty selectedRound;
 
+    private ObjectProperty<PairingNode> selectedPairingNode;
     private ObjectProperty<Pairing> selectedPairing;
 
     public PairingView() {
-	try {
-	    FXMLLoader loader = new FXMLLoader(this.getClass().getResource(
-		    "/ui/fxml/controls/pairing-view.fxml"));
-	    loader.setController(this);
-	    loader.setRoot(this);
-	    loader.load();
-	} catch (IOException e) {
-	    log.log(Level.SEVERE, e.getMessage(), e);
-	}
+        try {
+            FXMLLoader loader = new FXMLLoader(this.getClass().getResource(
+                    "/ui/fxml/controls/pairing-view.fxml"));
+            loader.setController(this);
+            loader.setRoot(this);
+            loader.load();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
+
+        this.selectedPairingNode = new SimpleObjectProperty<>();
+        this.selectedPairingNode.addListener((ov, o, n) -> {
+            if (n != null) {
+                this.setSelectedPairing(n.getPairing());
+            } else {
+                this.setSelectedPairing(null);
+            }
+        });
     }
 
     @FXML
     private void initialize() {
-	this.SelectedRoundProperty().addListener(this::onSelectedRoundChanged);
+        this.SelectedRoundProperty().addListener(this::onSelectedRoundChanged);
     }
 
-    private void onSelectedRoundChanged(ObservableValue<? extends Number> ov,
-	    Number o, Number n) {
-	// clear the selected pairing
-	this.setSelectedPairing(null);
+    private void onSelectedRoundChanged(
+            ObservableValue<? extends Number> observableValue, Number oldValue,
+            Number newValue) {
+        // clear the selected pairing node
+        this.selectedPairingNode.set(null);
 
-	// clear the pairing container and add pairing nodes for every pairing
-	// there is in the selected round
-	TournamentRound round = this.loadedTournament.getRounds().get(
-		n.intValue());
-	this.pairingContainer.getChildren().clear();
-	for (int i = 0; i < round.getPairings().size(); i++) {
-	    this.pairingContainer.getChildren().add(
-		    this.createPairingNode(this.loadedTournament, round
-			    .getPairings().get(i), i));
-	}
+        // clear the pairing container and add pairing nodes for every pairing
+        // there is in the selected round
+        TournamentRound round = this.loadedTournament.getRounds().get(newValue.intValue());
+        this.pairingContainer.getChildren().clear();
+        for (int i = 0; i < round.getPairings().size(); i++) {
+            PairingNode pairingNode = new PairingNode(
+                    this.loadedTournament, round.getPairings().get(i), i);
+
+            pairingNode.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+                pairingNode.setSelected(true);
+                this.selectedPairingNode.set(pairingNode);
+            });
+
+            this.pairingContainer.getChildren().add(pairingNode);
+        }
     }
 
     @Override
     public void loadTournament(Tournament tournament) {
-	this.loadedTournament = tournament;
+        this.loadedTournament = tournament;
 
-	// automatically show newly added rounds
-	tournament.getRounds().addListener(this::onTournamentListChanged);
+        // automatically show newly added rounds
+        tournament.getRounds().addListener(this::onTournamentListChanged);
     }
 
     @Override
     public void unloadTournament() {
-	this.loadedTournament.getRounds().removeListener(
-		this::onTournamentListChanged);
-	this.loadedTournament = null;
+        this.loadedTournament.getRounds().removeListener(
+                this::onTournamentListChanged);
+        this.loadedTournament = null;
     }
 
     private void onTournamentListChanged(
-	    Change<? extends TournamentRound> change) {
-	if (change.next()) {
-	    this.setSelectedRound(this.loadedTournament.getRounds().size() - 1);
-	    this.refreshBreadcrumbs();
-	}
+            Change<? extends TournamentRound> change) {
+        if (change.next()) {
+            this.setSelectedRound(this.loadedTournament.getRounds().size() - 1);
+            this.refreshBreadcrumbs();
+        }
     }
 
     /**
      * Refreshes the buttons in the breadcrumb bar.
      */
     private void refreshBreadcrumbs() {
-	// throw out all old breadcrumb buttons
-	this.breadcrumbContainer.getChildren().clear();
+        // throw out all old breadcrumb buttons
+        this.breadcrumbContainer.getChildren().clear();
 
-	int roundCount = this.loadedTournament.getRounds().size();
-	for (int roundNumber = 0; roundNumber < roundCount; roundNumber++) {
-	    // Users don't like zero-based indices
-	    Button breadcrumb = new Button("Runde " + (roundNumber + 1));
-	    final int selectRound = roundNumber;
+        int roundCount = this.loadedTournament.getRounds().size();
+        for (int roundNumber = 0; roundNumber < roundCount; roundNumber++) {
+            // Users don't like zero-based indices
+            Button breadcrumb = new Button("Runde " + (roundNumber + 1));
+            final int selectRound = roundNumber;
 
-	    // make the button select the correct round
-	    breadcrumb.setOnAction(event -> {
-		this.setSelectedRound(selectRound);
-	    });
+            // make the button select the correct round
+            breadcrumb.setOnAction(event -> {
+                this.setSelectedRound(selectRound);
+            });
 
-	    // assign the correct style classes to our breadcrumb button
-	    breadcrumb.getStyleClass().add("breadcrumb-button");
+            // assign the correct style classes to our breadcrumb button
+            breadcrumb.getStyleClass().add("breadcrumb-button");
 
-	    // this is our only button, we don't need additional style classes
-	    if (roundCount < 1) {
-		return;
-	    }
+            // this is our only button, we don't need additional style classes
+            if (roundCount < 1) {
+                return;
+            }
 
-	    if (roundNumber == 0) {
-		// this is our left-most breadcrumb
-		breadcrumb.getStyleClass().add("left");
-	    } else if (roundNumber == roundCount - 1) {
-		// this is our right-most breadcrumb
-		breadcrumb.getStyleClass().add("right");
-	    } else {
-		// seems like we're somewhere in-between left and right
-		breadcrumb.getStyleClass().add("middle");
-	    }
+            if (roundNumber == 0) {
+                // this is our left-most breadcrumb
+                breadcrumb.getStyleClass().add("left");
+            } else if (roundNumber == roundCount - 1) {
+                // this is our right-most breadcrumb
+                breadcrumb.getStyleClass().add("right");
+            } else {
+                // seems like we're somewhere in-between left and right
+                breadcrumb.getStyleClass().add("middle");
+            }
 
-	    // add the breadcrumb to the breadcrumb bar
-	    this.breadcrumbContainer.getChildren().add(breadcrumb);
-	}
-    }
-
-    private Node createPairingNode(Tournament tournament, Pairing pairing,
-	    int index) {
-	VBox pairingNode = new VBox(5);
-
-	pairingNode.getChildren().add(new Label("#" + index));
-
-	TableView<PlayerScore> opponentTable = new TableView<>();
-	opponentTable.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
-	    this.setSelectedPairing(pairing);
-	});
-	TableColumn<PlayerScore, String> playerNameColumn = new TableColumn<>(
-		"Name");
-	playerNameColumn.setCellValueFactory(score -> {
-	    return score.getValue().getPlayer().lastNameProperty();
-	});
-	opponentTable.getColumns().add(playerNameColumn);
-	for (PossibleScoring scoring : tournament.getRuleSet()
-		.getPossibleScores()) {
-	    TableColumn<PlayerScore, String> scoreColumn = new TableColumn<>(
-		    Integer.toString(scoring.getPriority()));
-	    scoreColumn.setCellValueFactory(score -> {
-		return new SimpleStringProperty(Integer.toString(score
-			.getValue().getScore().get(scoring.getPriority())));
-	    });
-	}
-
-	return pairingNode;
+            // add the breadcrumb to the breadcrumb bar
+            this.breadcrumbContainer.getChildren().add(breadcrumb);
+        }
     }
 
     /**
      * @return the SelectedRound property
      */
     public IntegerProperty SelectedRoundProperty() {
-	if (this.selectedRound == null) {
-	    this.selectedRound = new SimpleIntegerProperty(-1);
-	}
-	return this.selectedRound;
+        if (this.selectedRound == null) {
+            this.selectedRound = new SimpleIntegerProperty(-1);
+        }
+        return this.selectedRound;
     }
 
     /**
      * @return the value of the SelectedRound property
      */
     public int getSelectedRound() {
-	return this.SelectedRoundProperty().get();
+        return this.SelectedRoundProperty().get();
     }
 
     /**
-     * @param value
-     *            sets the new value for the SelectedRound property
+     * @param value sets the new value for the SelectedRound property
      */
     public void setSelectedRound(int value) {
-	this.SelectedRoundProperty().set(value);
+        this.SelectedRoundProperty().set(value);
     }
 
     /**
      * @return the selectedPairing property
      */
     public ObjectProperty<Pairing> selectedPairingProperty() {
-	if (this.selectedPairing == null) {
-	    this.selectedPairing = new SimpleObjectProperty<>();
-	}
-	return this.selectedPairing;
+        if (this.selectedPairing == null) {
+            this.selectedPairing = new SimpleObjectProperty<>();
+        }
+        return this.selectedPairing;
     }
 
     /**
      * @return the value of the selectedPairing property
      */
     public Pairing getSelectedPairing() {
-	return this.selectedPairingProperty().get();
+        return this.selectedPairingProperty().get();
     }
 
     /**
@@ -229,6 +202,6 @@ public class PairingView extends VBox implements TournamentUser {
      *            sets the new value for the selectedPairing property
      */
     private void setSelectedPairing(Pairing value) {
-	this.selectedPairingProperty().set(value);
+        this.selectedPairingProperty().set(value);
     }
 }
