52dcc60573d17cd0d3b77d4a430d3eca567e682c
Fabian Biester <fabian@familie-biester.de>
2015-02-02 23:57:15 +0100

    fixed a bug in the comparator

--- Application/src/usspg31/tourney/model/PlayerScoreComperator.java
+++ Application/src/usspg31/tourney/model/PlayerScoreComperator.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class PlayerScoreComperator implements Comparator<PlayerScore> {
     @Override
     public int compare(PlayerScore o1, PlayerScore o2) {
-        if (Integer.valueOf(o1.getPlayer().getStartingNumber()) == Integer
-                .valueOf(o2.getPlayer().getStartingNumber())) {
+        if (o1.getPlayer().getStartingNumber()
+                .equals(o2.getPlayer().getStartingNumber())) {
             return 0;
         } else if (Integer.valueOf(o1.getPlayer().getStartingNumber()) > Integer
                 .valueOf(o2.getPlayer().getStartingNumber())) {
