2006bfe4b5d8fc64e752d9d0f9c9a8767f4e84a2
Jan Tagscherer <jan.tagscherer@gmail.com>
2015-02-10 21:53:48 +0100

    Fixed small bug concerning the administrators

--- Application/src/usspg31/tourney/model/EventAdministrator.java
+++ Application/src/usspg31/tourney/model/EventAdministrator.java
@@ -4,5 +4,15 @@ package usspg31.tourney.model;
  * Represents an administrator that manages an event
  */
 public class EventAdministrator extends Administrator {
+    @Override
+    public Object clone() {
+        EventAdministrator clone = new EventAdministrator();
 
+        clone.setFirstName(this.getFirstName());
+        clone.setLastName(this.getLastName());
+        clone.setMailAdress(this.getMailAddress());
+        clone.setPhoneNumber(this.getPhoneNumber());
+
+        return clone;
+    }
 }
--- Application/src/usspg31/tourney/model/TournamentAdministrator.java
+++ Application/src/usspg31/tourney/model/TournamentAdministrator.java
@@ -4,5 +4,15 @@ package usspg31.tourney.model;
  * Represents a tournament administrator who runs a tournament
  */
 public class TournamentAdministrator extends Administrator {
+    @Override
+    public Object clone() {
+        TournamentAdministrator clone = new TournamentAdministrator();
 
+        clone.setFirstName(this.getFirstName());
+        clone.setLastName(this.getLastName());
+        clone.setMailAdress(this.getMailAddress());
+        clone.setPhoneNumber(this.getPhoneNumber());
+
+        return clone;
+    }
 }
